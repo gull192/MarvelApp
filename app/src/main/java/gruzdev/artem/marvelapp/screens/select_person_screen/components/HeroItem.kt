758b76eea5021aa8866e09cada440fd226c80959
Artem Gruzdev <myposhta.ruzdev@gmail.com>
2022-10-31 20:21:33 +0600

    add asyncImage

--- app/src/main/java/gruzdev/artem/marvelapp/screens/select_person_screen/components/HeroItem.kt
+++ app/src/main/java/gruzdev/artem/marvelapp/screens/select_person_screen/components/HeroItem.kt
@@ -1,36 +1,30 @@
 package gruzdev.artem.marvelapp.screens.select_person_screen.components
 
-import android.graphics.drawable.PaintDrawable
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.Paint
-import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.imageResource
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import gruzdev.artem.marvelapp.screens.select_person_screen.model.HeroCard
-import gruzdev.artem.marvelapp.ui.theme.Shapes
 import gruzdev.artem.marvelapp.ui.theme.Typography
 
 @Composable
 fun HeroItem (heroCard: HeroCard, modifier: Modifier = Modifier) {
     Box (modifier = modifier) {
-        Image(painter = painterResource(heroCard.image),
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(heroCard.photoURL)
+                .crossfade(true)
+                .build(),
             contentDescription = null,
-            modifier = Modifier
-                .fillMaxSize()
-                .clip(Shapes.large),
-            contentScale = ContentScale.Crop
+            contentScale = ContentScale.Crop,
+            modifier = modifier.fillMaxSize()
         )
         Text(
             text = heroCard.title,
