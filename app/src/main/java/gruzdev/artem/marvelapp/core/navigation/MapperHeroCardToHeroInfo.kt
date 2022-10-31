22714bbe710fbe985c42833c059de75275366123
Artem Gruzdev <myposhta.ruzdev@gmail.com>
2022-10-31 20:21:49 +0600

    add navigation

---
+++ app/src/main/java/gruzdev/artem/marvelapp/core/navigation/MapperHeroCardToHeroInfo.kt
@@ -0,0 +1,12 @@
+package gruzdev.artem.marvelapp.core.navigation.model
+
+import gruzdev.artem.marvelapp.core.model.HeroInfo
+import gruzdev.artem.marvelapp.screens.select_person_screen.model.HeroCard
+
+fun HeroCard.asHeroInfo(): HeroInfo {
+    return HeroInfo (
+        heroName = title,
+        photoUrl = photoURL,
+        descriptionHero = descriptionHero
+    )
+}
